56fa63d6f6a2d1da8c464ee40dff1e1906f9deb7
Babu <babusaran11@gmail.com>
2018-05-08 13:55:59 +0530

    Implemented Elmah Error Log

---
+++ MortgageCalculator.UnitTest/MvcController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MortgageCalculator.Web.Controllers;
+using System.Web.Mvc;
+
+namespace MortgageCalculator.UnitTest
+{
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [Ignore] // Ignored since the ip is refused to create a connection with the host
+        [TestMethod]
+        public void HomeController_Valid()
+        {
+            HomeController controller = new HomeController();
+
+            var result = controller.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+            //Assert.AreEqual("Index", result.ViewName);
+
+        }
+    }
+
+}
